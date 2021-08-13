d5530e40006df3f04d6a16b826788f06c345948b
George <gzald001@gmail.com>
2021-08-13 18:42:36 -0400

    feat: Add IIFE that returns array of dino objects with properites

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-
     // Create Dino Constructor
-
-
     // Create Dino Objects
-
+    let dinosaurs = (function(){
+        return dinoData["Dinos"];
+    })();
 
     // Create Human Object
 
@@ -11,19 +10,19 @@
 
 
     // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
+
         // Add tiles to DOM
 
     // Remove form from screen
