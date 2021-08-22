7c7587b704bc34da26dd850fe9c98ce4be00935b
George <gzald001@gmail.com>
2021-08-21 22:32:18 -0400

    feat: Canvas method create tile

--- app.js
+++ app.js
@@ -61,6 +61,33 @@ button.addEventListener('click', function(){
         return [this.weight, this.height, this.diet, this.where,
              this.when, this.fact];
     }
+
+    // Generate Tiles for each Dino in Array
+    Canvas.prototype.createTile = function(animal){
+        const species = animal.species;
+        const main = document.getElementById('grid');
+        const div = document.createElement('div');
+        const header = document.createElement('h3');
+
+        if(animal.species !== 'Human'){
+            const paragraph = document.createElement('p');
+            paragraph.setAttribute('id', species + 'fact');
+            div.appendChild(paragraph);
+        }
+
+        header.setAttribute('id', species);
+        div.setAttribute('class', 'grid-item');
+        div.appendChild(header);
+
+        const image = document.createElement('img');
+        image.setAttribute('id', species + 'img');
+        div.appendChild(image);
+        main.appendChild(div);
+    }
+
+
+
+
 });
 
     // Create Dino Compare Method 1
@@ -75,7 +102,7 @@ button.addEventListener('click', function(){
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
-    // Generate Tiles for each Dino in Array
+
 
         // Add tiles to DOM
 
